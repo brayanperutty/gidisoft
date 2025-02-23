abdb29306f33d50873d33a916eedf959fa27730b
peruttynelumbo <b.perutty@nelumbo.com.co>
2025-02-23 18:50:18 -0500

    Initial structure project

---
+++ src/main/java/com/ufps/gidisoft/GidisoftApplication.java
@@ -0,0 +1,13 @@
+package com.ufps.gidisoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GidisoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GidisoftApplication.class, args);
+	}
+
+}
